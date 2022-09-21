f884c32d537d224f427b39705c31096190140ed8
syed abdul hakim <s.abdulhakim10@gmail.com>
2022-09-22 01:30:08 +0600

    api data load practice in react

--- src/App.js
+++ src/App.js
@@ -1,13 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+      <ExternalUsers></ExternalUsers>
     </div>
   );
+}
+
+function ExternalUsers (){
+  // first step
+  const [users, setUsers] = useState([]);
+
+  // second step
+  useEffect( () => {
+  
+  // third step
+  fetch('https://jsonplaceholder.typicode.com/users')
+  .then(res => res.json())
+  .then(data => setUsers(data))
+
+  }, [])
+  return (
+    <div>
+      <h2>External Users</h2>
+      <p>{users.length}</p>
+      {
+        users.map (user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props){
+  return (
+    <div style={{border:'2px solid purple', margin: '10px', borderRadius: '10px', backgroundColor: 'skyblue'}}>
+      <h3>Name: {props.name} </h3>
+      <p>Email: {props.email}</p>
+    </div>
+  )
 }
 
 function Counter(){
